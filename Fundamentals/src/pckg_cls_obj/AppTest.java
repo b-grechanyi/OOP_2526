f38b0c2ce1870e21e8a027a70263d480e5fdb054
b-grechanyi <b.grechanyi@gmail.com>
2026-03-09 18:16:36 +0100

    Class Student is Defined.

---
+++ Fundamentals/src/pckg_cls_obj/AppTest.java
@@ -0,0 +1,11 @@
+package pckg_cls_obj;
+
+public class AppTest {
+
+    public static void main(String[] arg){
+        Student s1 = new Student("Petar", "Petric", 12);
+        Student s2 = new Student("Franka", "Franic", 43);
+        s1.subscribeToStudentService();
+    }
+
+}
